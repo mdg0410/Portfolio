027181a67c696bc8e3fdf8c39f2a65cf52bb86a0
mdg0410 <levoyersteven@gmail.com>
2023-02-06 16:27:05 -0500

    Add menu deploy

---
+++ script.js
@@ -0,0 +1,11 @@
+let sidebar = document.querySelector('#sideBar');
+
+document.querySelector('.openMenu').addEventListener('click', () => {
+  sidebar.style.width="375px";
+});
+document.querySelector('.closeMenu').addEventListener('click', () => {
+  sidebar.style.width="0";
+});
+document.querySelector('.menu-items').addEventListener('click', () => {
+  sidebar.style.width="0";
+});
