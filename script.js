c5db8dbf1911e581d5af46d75c8ceb713804606d
mdg0410 <levoyersteven@gmail.com>
2023-02-06 16:33:45 -0500

    Add linter ESLint files

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let sidebar = document.querySelector('#sideBar');
+const sidebar = document.querySelector('#sideBar');
 
 document.querySelector('.openMenu').addEventListener('click', () => {
-  sidebar.style.width="375px";
+  sidebar.style.width = '375px';
 });
 document.querySelector('.closeMenu').addEventListener('click', () => {
-  sidebar.style.width="0";
+  sidebar.style.width = '0';
 });
 document.querySelector('.menu-items').addEventListener('click', () => {
-  sidebar.style.width="0";
+  sidebar.style.width = '0';
 });
