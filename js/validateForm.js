5d36c5ebfb230f816f2a59f51c8646592368c0c8
mdg0410 <levoyersteven@gmail.com>
2023-02-10 15:02:47 -0500

    Add validation email with javaScript file

--- js/validateForm.js
+++ js/validateForm.js
@@ -1,11 +1,15 @@
 const nameEl = document.querySelector('#name');
+const emailEl = document.querySelector('#e-mail');
 const textEl = document.querySelector('#message');
-const addSmall = document.createElement('small');
-
+const btnSubmit = document.querySelector('#formSubmit');
 const form = document.querySelector('#formContact');
 
 const isRequired = (value) => (value !== '');
 const isBetween = (length, min, max) => (!(length < min || length > max));
+const isEmailValid = (email) => {
+  const re = /^(([a-z]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
+  return re.test(email);
+};
 
 const showError = (input, message) => {
   // get the form-field element
@@ -15,7 +19,6 @@ const showError = (input, message) => {
   input.classList.add('error');
 
   // show the error message
-  formField.appendChild(addSmall);
   const error = formField.querySelector('small');
   error.textContent = message;
 };
@@ -23,22 +26,36 @@ const showError = (input, message) => {
 const showSuccess = (input) => {
   // get the form-field element
   const formField = input.parentElement;
-
-  // remove the error class
-  input.classList.add('success');
+  // add the error class
   input.classList.remove('error');
+  input.classList.add('success');
 
-  if (formField.querySelector('small' !== '')) {
-    const deleteSmall = formField.querySelector('small');
-    deleteSmall.remove();
-  }
+  // show the error message
+  const error = formField.querySelector('small');
+  error.textContent = '';
+};
+
+const showErrorbtn = (input, message) => {
+  // get the form-field element
+  const formField = input.parentElement;
+  // show the error message
+  const error = formField.querySelector('small');
+  input.style.border = 'red solid 1px';
+  error.textContent = message;
+};
+
+const btnSuccess = (input) => {
+  // get the form-field element
+  const formField = input.parentElement;
+  // show the error message
+  input.style.border = 'none';
+  const error = formField.querySelector('small');
+  error.textContent = '';
 };
 
 const checkName = () => {
   let valid = false;
-
-  const min = 3; const max = 25;
-
+  const min = 3; const max = 30;
   const name = nameEl.value.trim();
 
   if (!isRequired(name)) {
@@ -52,15 +69,27 @@ const checkName = () => {
   return valid;
 };
 
+const checkEmail = () => {
+  let valid = false;
+  const email = emailEl.value.trim();
+  if (!isRequired(email)) {
+    showError(emailEl, 'Email cannot be blank.');
+  } else if (!isEmailValid(email)) {
+    showError(emailEl, 'The email is invalid, try to write it in lowercase.');
+  } else {
+    showSuccess(emailEl);
+    valid = true;
+  }
+  return valid;
+};
+
 const checkText = () => {
   let valid = false;
-
   const min = 3; const max = 250;
-
   const text = textEl.value.trim();
 
   if (!isRequired(text)) {
-    showError(textEl, 'Username cannot be blank.');
+    showError(textEl, 'Text cannot be blank.');
   } else if (!isBetween(text.length, min, max)) {
     showError(textEl, `Text must be between ${min} and ${max} characters.`);
   } else {
@@ -71,17 +100,29 @@ const checkText = () => {
 };
 
 form.addEventListener('click', (e) => {
+  // prevent the form from submitting
+
   // validate fields
   const isUsernameValid = checkName();
+  const isEmailValid = checkEmail();
   const isTextValid = checkText();
 
   const isFormValid = isUsernameValid
+        && isEmailValid
         && isTextValid;
 
   // submit to the server if the form is valid
-  if (isFormValid === false) {
-    // prevent the form from submitting
+  if (!isFormValid) {
     e.preventDefault();
+    if (!isUsernameValid) {
+      showErrorbtn(btnSubmit, 'Error in name');
+    } else if (!isEmailValid) {
+      showErrorbtn(btnSubmit, 'Error in email');
+    } else if (!isTextValid) {
+      showErrorbtn(btnSubmit, 'Error in text');
+    }
+  } else {
+    btnSuccess(btnSubmit);
   }
 });
 
@@ -103,6 +144,9 @@ const debounce = (fn, delay = 500) => {
 form.addEventListener('input', debounce((e) => {
   switch (e.target.id) {
     case 'name':
+      checkName();
+      break;
+    case 'e-mail':
       checkName();
       break;
     case 'message':
