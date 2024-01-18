f5c9571477f655b3247a3099451c3371f6eaf930
mdg0410 <levoyersteven@gmail.com>
2024-01-17 22:40:24 -0500

    Update linters files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Asegúrate de que este sea el punto de entrada de tu aplicación
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+};
