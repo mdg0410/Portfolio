fb1cf9b5eff53934a5031c5201e98226e1f809f6
mdg0410 <levoyersteven@gmail.com>
2024-01-26 13:28:40 -0500

    Add modules css at the proyect

--- src/components/sidebarComponent.jsx
+++ src/components/sidebarComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleSidebar, selectSidebarState } from '../redux/slices/sidebarSlice';
-import '../styles/sidebar.css';
+import styles from '../styles/portfolio.module.css';
 
 const SidebarComponent = () => {
   const dispatch = useDispatch();
@@ -10,18 +10,20 @@ const SidebarComponent = () => {
   return (
     <>
       {!isSidebarOpen ? (
-        <button type="button" onClick={() => dispatch(toggleSidebar())}>
+        <button className={styles.menuButton} type="button" onClick={() => dispatch(toggleSidebar())}>
           Menú
         </button>
       ) : (
-        <div className="sidebar">
-          <button type="button" onClick={() => dispatch(toggleSidebar())}>
-            X
-          </button>
+        <div className={styles.sidebar}>
+          <div className={styles.closeContainer}>
+            <button className={styles.closeButton} type="button" onClick={() => dispatch(toggleSidebar())}>
+              X
+            </button>
+          </div>
           <ul>
-            <li><a href="#Works">Works</a></li>
-            <li><a href="#About">About</a></li>
-            <li><a href="#Contact">Contact</a></li>
+            <li><a href="#Works" onClick={() => dispatch(toggleSidebar())}>Works</a></li>
+            <li><a href="#About" onClick={() => dispatch(toggleSidebar())}>About</a></li>
+            <li><a href="#Contact" onClick={() => dispatch(toggleSidebar())}>Contact</a></li>
           </ul>
         </div>
       )}
