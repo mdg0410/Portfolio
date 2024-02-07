05bc347eeac53c860ce5af79bf9042d5037d1dbe
mdg0410 <levoyersteven@gmail.com>
2024-02-06 20:44:55 -0500

    Actualiza estilos de CSS en archivos headline.module.css y about.module.css y corrige descripción del proyecto

--- src/components/aboutComponent.jsx
+++ src/components/aboutComponent.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectAbout, updateField } from '../redux/slices/aboutSlice';
 import styles from '../styles/about.module.css';
-import { ReactComponent as JavaScript } from '../assets/Icons/javascript.svg';
-import { ReactComponent as Html } from '../assets/Icons/html.svg';
-import { ReactComponent as Css } from '../assets/Icons/css.svg';
 import { ReactComponent as Close } from '../assets/Icons/v-90.svg';
 import { ReactComponent as Open } from '../assets/Icons/v.svg';
 
@@ -31,26 +28,26 @@ const About = () => {
               <a href="#Contact" className={styles.letsConnect}>LET’S CONNECT</a>
             </div>
             <div className={styles.frame1}>
-            <ul className={styles.headerIcons}>
-          <li className={styles.smIcons}>
-            <a href="mailto:levoyersteven@gmail.com"><img alt="Gmail" src="https://raw.githubusercontent.com/gauravghongde/social-icons/9d939e1c5b7ea4a24ac39c3e4631970c0aa1b920/SVG/Black/Gmail_black.svg" /></a>
-          </li>
-          <li className={styles.smIcons}>
-            <a href="https://www.linkedin.com/in/richard-steven-levoyer-chavez-9b902525b/"><img alt="LinkedIn" src="https://raw.githubusercontent.com/gauravghongde/social-icons/9d939e1c5b7ea4a24ac39c3e4631970c0aa1b920/SVG/Black/LinkedIN_black.svg" /></a>
-          </li>
-          <li className={styles.smIcons}>
-            <a href="https://wa.me/tunumerodetelefono/?text=¡Hola!%20😊%20Si%20tienes%20alguna%20pregunta,%20necesitas%20ayuda,%20o%20simplemente%20quieres%20charlar,%20estoy%20aquí%20para%20ayudarte.%20¡No%20dudes%20en%20escribirme%20en%20WhatsApp!%20🚀📱" target="_blank" rel="noreferrer"><img alt="WhastApp" src="https://raw.githubusercontent.com/gauravghongde/social-icons/9d939e1c5b7ea4a24ac39c3e4631970c0aa1b920/SVG/Black/WhatsApp_black.svg" /></a>
-          </li>
-          <li className={styles.smIcons}>
-            <a href="https://mdg0410.slack.com/archives/C06HCBB01AN"><img alt="Slack" src="https://raw.githubusercontent.com/gauravghongde/social-icons/9d939e1c5b7ea4a24ac39c3e4631970c0aa1b920/SVG/Black/Slack_black.svg" /></a>
-          </li>
-          <li className={styles.smIcons}>
-            <a href="https://github.com/mdg0410"><img alt="GitHub" src="https://raw.githubusercontent.com/gauravghongde/social-icons/9d939e1c5b7ea4a24ac39c3e4631970c0aa1b920/SVG/Black/Github_black.svg" /></a>
-          </li>
-        </ul>
+              <ul className={styles.headerIcons}>
+                <li className={styles.smIcons}>
+                  <a href="mailto:levoyersteven@gmail.com"><img alt="Gmail" src="https://raw.githubusercontent.com/gauravghongde/social-icons/9d939e1c5b7ea4a24ac39c3e4631970c0aa1b920/SVG/Black/Gmail_black.svg" /></a>
+                </li>
+                <li className={styles.smIcons}>
+                  <a href="https://www.linkedin.com/in/richard-steven-levoyer-chavez-9b902525b/"><img alt="LinkedIn" src="https://raw.githubusercontent.com/gauravghongde/social-icons/9d939e1c5b7ea4a24ac39c3e4631970c0aa1b920/SVG/Black/LinkedIN_black.svg" /></a>
+                </li>
+                <li className={styles.smIcons}>
+                  <a href="https://wa.me/tunumerodetelefono/?text=¡Hola!%20😊%20Si%20tienes%20alguna%20pregunta,%20necesitas%20ayuda,%20o%20simplemente%20quieres%20charlar,%20estoy%20aquí%20para%20ayudarte.%20¡No%20dudes%20en%20escribirme%20en%20WhatsApp!%20🚀📱" target="_blank" rel="noreferrer"><img alt="WhastApp" src="https://raw.githubusercontent.com/gauravghongde/social-icons/9d939e1c5b7ea4a24ac39c3e4631970c0aa1b920/SVG/Black/WhatsApp_black.svg" /></a>
+                </li>
+                <li className={styles.smIcons}>
+                  <a href="https://mdg0410.slack.com/archives/C06HCBB01AN"><img alt="Slack" src="https://raw.githubusercontent.com/gauravghongde/social-icons/9d939e1c5b7ea4a24ac39c3e4631970c0aa1b920/SVG/Black/Slack_black.svg" /></a>
+                </li>
+                <li className={styles.smIcons}>
+                  <a href="https://github.com/mdg0410"><img alt="GitHub" src="https://raw.githubusercontent.com/gauravghongde/social-icons/9d939e1c5b7ea4a24ac39c3e4631970c0aa1b920/SVG/Black/Github_black.svg" /></a>
+                </li>
+              </ul>
             </div>
           </div>
-          <button type="button" className={styles.actionButton}>Get my resume</button>
+          <a href="https://github.com/mdg0410" className={styles.actionButton}>Get my resume</a>
         </div>
         <ul className={styles.skillList}>
           <li>
@@ -68,16 +65,13 @@ const About = () => {
             isOpen.languajes && (
               <div className={styles.languagesList}>
                 <div className={styles.frameList}>
-                  <span><JavaScript /></span>
-                  <span className={styles.frameText}>JavaScript</span>
+                  <span><img alt="JavaScript" src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E" /></span>
                 </div>
                 <div className={styles.frameList}>
-                  <span><Html /></span>
-                  <span className={styles.frameText}>HTML</span>
+                  <span><img alt="HTML" src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" /></span>
                 </div>
                 <div className={styles.frameList}>
-                  <span><Css /></span>
-                  <span className={styles.frameText}>CSS</span>
+                  <span><img alt="CSS" src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" /></span>
                 </div>
               </div>
             )
@@ -99,16 +93,16 @@ const About = () => {
             isOpen.frameworks && (
             <div className={styles.languagesList}>
               <div className={styles.frameList}>
-                <span><JavaScript /></span>
-                <span className={styles.frameText}>React.js</span>
+                <span><img alt="React" src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" /></span>
               </div>
               <div className={styles.frameList}>
-                <span><Html /></span>
-                <span className={styles.frameText}>Bootstrap</span>
+                <span><img alt="React Routes" src="https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white" /></span>
               </div>
               <div className={styles.frameList}>
-                <span><Css /></span>
-                <span className={styles.frameText}>jQuery</span>
+                <span><img alt="Redux" src="https://img.shields.io/badge/Redux-593D88?style=for-the-badge&logo=redux&logoColor=white" /></span>
+              </div>
+              <div className={styles.frameList}>
+                <span><img alt="Bootstrap" src="https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white" /></span>
               </div>
             </div>
             )
@@ -130,24 +124,16 @@ const About = () => {
             isOpen.skills && (
             <div className={styles.languagesList}>
               <div className={styles.frameList}>
-                <span><JavaScript /></span>
-                <span className={styles.frameText}>Trabajo en Equipo</span>
+                <span><img alt="Babel" src="https://img.shields.io/badge/Babel-F9DC3E?style=for-the-badge&logo=babel&logoColor=white" /></span>
               </div>
               <div className={styles.frameList}>
-                <span><Html /></span>
-                <span className={styles.frameText}>Resolución de Problemas</span>
+                <span><img alt="Firebase" src="https://img.shields.io/badge/firebase-ffca28?style=for-the-badge&logo=firebase&logoColor=black" /></span>
               </div>
               <div className={styles.frameList}>
-                <span><Css /></span>
-                <span className={styles.frameText}>Pensamiento Analítico</span>
+                <span><img alt="Jest" src="https://img.shields.io/badge/Jest-C21325?style=for-the-badge&logo=jest&logoColor=white" /></span>
               </div>
               <div className={styles.frameList}>
-                <span><Html /></span>
-                <span className={styles.frameText}>Aprendizaje Continuo</span>
-              </div>
-              <div className={styles.frameList}>
-                <span><Css /></span>
-                <span className={styles.frameText}>Adaptabilidad</span>
+                <span><img alt="Webpack" src="https://img.shields.io/badge/Webpack-8DD6F9?style=for-the-badge&logo=Webpack&logoColor=white" /></span>
               </div>
             </div>
             )
